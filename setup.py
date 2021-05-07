1d190860366f032df5abeb33d63a5a9c85a91f5c
gianpaolocombatti <gianpaolocombatti@Gianpaolos-MacBook-Pro.local>
2021-05-07 00:02:03 -0400

    nex commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+# Always prefer setuptools over distutils
+from setuptools import setup
+setup(
+    name='dspt11_assignment_gpc',  # Required
+    version='0.0.1',  # Required
+    author='dspt11',  # Optional
+    author_email='gianpaolofcombatti@gmail.com',  # Optional
+    keywords='assignment',  # Optional
+    packages=['mymodule'],  # Required
+    python_requires='>=3.6, <4',
+    install_requires=['pandas','numpy'],  # Optional
+)
